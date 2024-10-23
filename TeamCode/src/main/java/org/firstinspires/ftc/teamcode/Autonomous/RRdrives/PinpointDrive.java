c3cd7afa774676020bff47350d9ea4fe26f506f6
RajarshiBandhu <97370250+RajarshiBandhu@users.noreply.github.com>
2024-10-22 22:11:10 -0500

    april tag re arranged

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRdrives/PinpointDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRdrives/PinpointDrive.java
@@ -35,14 +35,6 @@ public class PinpointDrive extends MecanumDrive {
         public double encoderResolution = GoBildaPinpointDriverRR.goBILDA_4_BAR_POD;
         public GoBildaPinpointDriver.EncoderDirection xDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
         public GoBildaPinpointDriver.EncoderDirection yDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
-        final double cameraPlacementX = 0;
-        final double cameraPlacementY = 7.4;
-        final double cameraAngle = Math.atan(cameraPlacementY/cameraPlacementX);
-        final double botCenterHypotenuse = Math.sqrt(Math.pow(cameraPlacementX,2) + Math.pow(cameraPlacementY,2));
-        public Vector2d targetAprilTag;
-    }
-    public enum corners{
-        blueBucket, blueSpecimen, redBucket, redSpecimen
     }
 
     public static Params PARAMS = new Params();
@@ -66,31 +58,7 @@ public class PinpointDrive extends MecanumDrive {
             throw new RuntimeException(e);
         }
         pinpoint.setPosition(pose);
-        limelight = new Limelight(hardwareMap);
-    }
-    public PinpointDrive(HardwareMap hardwareMap, Pose2d pose, corners corner) {
-        super(hardwareMap, pose);
-        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        FlightRecorder.write("PINPOINT_PARAMS",PARAMS);
-        pinpoint = hardwareMap.get(GoBildaPinpointDriverRR.class,"pinpoint");
-        pinpoint.setOffsets(DistanceUnit.MM.fromInches(PARAMS.xOffset), DistanceUnit.MM.fromInches(PARAMS.yOffset));
-        pinpoint.setEncoderResolution(PARAMS.encoderResolution);
-        pinpoint.setEncoderDirections(PARAMS.xDirection, PARAMS.yDirection);
-        pinpoint.resetPosAndIMU();
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        pinpoint.setPosition(pose);
-        limelight = new Limelight(hardwareMap);
-
-        switch (corner){
-            case blueBucket:
-                PARAMS.targetAprilTag = new Vector2d(20,40);
-                break;
-        }
+        limelight = new Limelight(hardwareMap, Limelight.corners.blueBucket);
     }
 
     @Override
@@ -102,19 +70,8 @@ public class PinpointDrive extends MecanumDrive {
         pose = pinpoint.getPositionRR();
 
         double heading = pose.heading.toDouble();
-        Pose2d aprilTagPose = UpdateAprilTag(Math.toDegrees(heading));
-        if (aprilTagPose != null){
-            Pose2d botTagPose = new Pose2d(
-                PARAMS.targetAprilTag.x + aprilTagPose.position.x,
-                PARAMS.targetAprilTag.y - aprilTagPose.position.y,
-                heading);
-            Pose2d weightedPose = new Pose2d(
-                (botTagPose.position.x + pose.position.x)/2,
-                (botTagPose.position.y + pose.position.y)/2,
-                heading
-            );
-            pose = weightedPose;
-        }
+        Pose2d aprilTagPose = limelight.getLatestPosition(Math.toDegrees(heading), pose);
+        if (aprilTagPose != null) pose = aprilTagPose;
 
         lastPinpointPose = pose;
 
@@ -124,25 +81,6 @@ public class PinpointDrive extends MecanumDrive {
         }
 
         return pinpoint.getVelocityRR();
-    }
-
-    public Pose2d UpdateAprilTag(double heading) {
-        Pose3D botpose = limelight.getLatestPosition(heading);
-
-        Pose2d newPose = null;
-        if (botpose != null) {
-            double cameraY = -((botpose.getPosition().x - 1.8002) / 0.04203)-85;
-            double cameraX = (((botpose.getPosition().y * 39.37) + 47.3044) / 1.65203)-58;
-
-            //if camera has y displacement from origin
-            double relativeBotX = Math.cos(Math.toRadians(heading) + PARAMS.cameraAngle) * PARAMS.botCenterHypotenuse;
-            double relativeBotY = Math.sin(Math.toRadians(heading) + PARAMS.cameraAngle) * PARAMS.botCenterHypotenuse;
-
-            double absoluteBotX = cameraX - relativeBotX;
-            double absoluteBotY = cameraY + relativeBotY;
-            newPose = new Pose2d(absoluteBotX, absoluteBotY, Math.toRadians(heading));
-        }
-        return newPose;
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Limelight.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes.FiducialResult;
 import com.qualcomm.hardware.limelightvision.LLStatus;
@@ -14,17 +16,57 @@ import java.util.List;
 
 public class Limelight {
     public Limelight3A limelight;
-    public Limelight(HardwareMap hardwareMap){
+    final double cameraPlacementX = 0;
+    final double cameraPlacementY = 7.4;
+    final double cameraAngle = Math.atan(cameraPlacementY/cameraPlacementX);
+    final double botCenterHypotenuse = Math.sqrt(Math.pow(cameraPlacementX,2) + Math.pow(cameraPlacementY,2));
+    public Vector2d targetAprilTag;
+    public enum corners{
+        blueBucket, blueSpecimen, redBucket, redSpecimen
+    }
+    public Limelight(HardwareMap hardwareMap, corners corner){
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
         limelight.start();
+
+        switch (corner){
+            case blueBucket:
+                targetAprilTag = new Vector2d(20,40);
+                break;
+        }
     }
 
-    public Pose3D getLatestPosition(double heading){
+    public Pose2d getLatestPosition(double heading, Pose2d pinpointPose){
+        Pose2d weightedPose = null;
+
         limelight.updateRobotOrientation(heading);
         LLResult result = limelight.getLatestResult();
-        if (!result.getFiducialResults().isEmpty()) return result.getBotpose_MT2();
-        return null;
+
+        if (!result.getFiducialResults().isEmpty()) {
+            Pose3D rawPose = result.getBotpose_MT2();
+
+            double cameraY = -((rawPose.getPosition().x - 1.8002) / 0.04203)-85;
+            double cameraX = (((rawPose.getPosition().y * 39.37) + 47.3044) / 1.65203)-58;
+
+            double relativeBotX = Math.cos(Math.toRadians(heading) + cameraAngle) * botCenterHypotenuse;
+            double relativeBotY = Math.sin(Math.toRadians(heading) + cameraAngle) * botCenterHypotenuse;
+
+            double absoluteBotX = cameraX - relativeBotX;
+            double absoluteBotY = cameraY + relativeBotY;
+            Pose2d distanceFromTag = new Pose2d(absoluteBotX, absoluteBotY, Math.toRadians(heading));
+
+            Pose2d botTagPose = new Pose2d(
+                    targetAprilTag.x + distanceFromTag.position.x,
+                    targetAprilTag.y - distanceFromTag.position.y,
+                    heading);
+
+            weightedPose = new Pose2d(
+                    (botTagPose.position.x + pinpointPose.position.x)/2,
+                    (botTagPose.position.y + pinpointPose.position.y)/2,
+                    heading
+            );
+        };
+        return weightedPose;
     }
 
 }
